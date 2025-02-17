ce305a4b9c236e19f9837cb0ab2950122926cf56
Daviif <103295181+Daviif@users.noreply.github.com>
2025-02-17 18:55:28 +0000

    Hora e data

--- main.java
+++ main.java
@@ -1,8 +1,16 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 public class main{
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
+        LocalDateTime atual = LocalDateTime.now();
+        DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+        String dataFormatada = formatterData.format(atual);
 
+        DateTimeFormatter formatterHora = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String horaFormatada = formatterHora.format(atual);
+        
         System.out.println("- - - Seja Bem-Vindo ao Sistema de Gestão de Bibliotecas - - -");
 
 
